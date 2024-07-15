b47f3a18150578a91c237b658926576111144ca1
Shivaraj Deyannavar <Shivaraj Deyannavar>
2024-07-15 21:33:37 +0530

    Hello world app

---
+++ hello-world/src/main/java/com/spring/boot/course/hello_world/controller/MyController.java
@@ -0,0 +1,13 @@
+package com.spring.boot.course.hello_world.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+    @DeleteMapping("helloworld")
+    public String helloWorld() {
+        return "Hello, World!";
+    }
+}
