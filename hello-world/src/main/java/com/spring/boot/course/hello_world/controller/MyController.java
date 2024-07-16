1e17635725e2d1cfcffd7da21ba97663ac4bc90e
Shivaraj Deyannavar <Shivaraj Deyannavar>
2024-07-16 16:05:56 +0530

    Actuators, CMD Line, Application properties

--- hello-world/src/main/java/com/spring/boot/course/hello_world/controller/MyController.java
+++ hello-world/src/main/java/com/spring/boot/course/hello_world/controller/MyController.java
@@ -1,13 +1,18 @@
 package com.spring.boot.course.hello_world.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyController {
+    @Value("${company.name}")
+    private String companyName;
+    @Value("${company.employee}")
+    private String employeeName;
     @RequestMapping("helloworld")
     public String helloWorld() {
-        return "Hello, World!";
+        return "Hello, " + employeeName + " from " + companyName;
     }
 }
