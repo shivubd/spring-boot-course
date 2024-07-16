a164342974f8e2d487581522565e65f1bc5e45f8
Shivaraj Deyannavar <Shivaraj Deyannavar>
2024-07-16 14:08:20 +0530

    Spring project structure, devtools

--- hello-world/src/main/java/com/spring/boot/course/hello_world/controller/MyController.java
+++ hello-world/src/main/java/com/spring/boot/course/hello_world/controller/MyController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyController {
-    @DeleteMapping("helloworld")
+    @RequestMapping("helloworld")
     public String helloWorld() {
         return "Hello, World!";
     }
